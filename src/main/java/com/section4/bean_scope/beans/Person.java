5a0d6fc2a52353531e52aa7341edbe09a79e0c98
Chanchal <rajguruchanchal@gmail.com>
2026-01-16 12:49:11 +0000

    Resolved conflicts

---
+++ src/main/java/com/section4/bean_scope/beans/Person.java
@@ -0,0 +1,15 @@
+package com.section4.bean_scope.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "personBean")
+public class Person {
+
+    private String name = "Lucy";
+    private final
+
+
+
+
+
+}
---
+++ src/main/java/com/section4/bean_scope/beans/Vehicle.java
@@ -0,0 +1,9 @@
+package com.section4.bean_scope.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component()
+public class Vehicle {
+
+
+}
